d7ee43daafdb340fab3f3020208d324141f12209
Justin Marney <gotascii@gmail.com>
2010-08-13 23:03:33 -0400

    initial rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glist::Application.config.secret_token = '86abdd9cf45f00cdaeab27a2164e7c34fa00f275179639c10b883f1fc66a140926c1a110ac5ea2847afd92563f558ccb9bea5524c351547bcedf44f0748192cb'
